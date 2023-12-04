199f6bfb2527b16beacb9c0ae5b15e3c1e032840
yczhou912 <yz4637@columbia.edu>
2023-12-04 12:46:58 -0500

    Check the datatype of 'geometry' columns

--- schema.sql
+++ schema.sql
@@ -1,28 +1,24 @@
-CREATE TABLE IF NOT EXISTS df_zipcode (
-    id INTEGER PRIMARY KEY,
-    zipcode TEXT,
+CREATE TABLE IF NOT EXISTS df_zipcode (    zipcode TEXT,
     PO_NAME TEXT,
     COUNTY TEXT,
-    geometry geometry(Geometry,2263)
-);CREATE TABLE df_311 (
-    id INTEGER PRIMARY KEY,
-    date TIMESTAMP,
+    geometry geometry(Geometry,2263),
+ PRIMARY KEY (id) 
+);CREATE TABLE IF NOT EXISTS df_311 (    date TIMESTAMP,
     complaint_type TEXT,
     zipcode TEXT,
     latitude TEXT,
     longitude TEXT,
-    geometry geometry(Geometry,2263)
-);CREATE TABLE df_tree (
-    id INTEGER PRIMARY KEY,
-    zipcode TEXT,
+    geometry geometry(Geometry,2263),
+ PRIMARY KEY (id) 
+);CREATE TABLE IF NOT EXISTS df_tree (    zipcode TEXT,
     latitude TEXT,
     longitude TEXT,
     x_sp TEXT,
     y_sp TEXT,
-    geometry geometry(Geometry,2263)
-);CREATE TABLE df_zillow (
-    id INTEGER PRIMARY KEY,
-    zipcode TEXT,
+    geometry geometry(Geometry,2263),
+ PRIMARY KEY (id) 
+);CREATE TABLE IF NOT EXISTS df_zillow (    zipcode TEXT,
     date TIMESTAMP,
-    rent REAL
+    rent REAL,
+ PRIMARY KEY (id) 
 );
